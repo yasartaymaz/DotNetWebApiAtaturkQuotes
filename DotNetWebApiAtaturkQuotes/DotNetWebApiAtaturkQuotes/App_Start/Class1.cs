050084b3f7042b329f6b94a99ad9585619d4d373
YasarTaymaz <yasartaymaz@windowslive.com>
2017-04-11 10:27:29 +0300

    ok. gitignore works :)

---
+++ DotNetWebApiAtaturkQuotes/DotNetWebApiAtaturkQuotes/App_Start/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DotNetWebApiAtaturkQuotes.App_Start
+{
+    public class Class1
+    {
+        public int MyProperty { get; set; }
+    }
+}
